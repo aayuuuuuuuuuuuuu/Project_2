96a55c790ec7fd6ad8219be140a0eb45fc56164f
aayuuuuuuuuuuu <aayushi.work20@gmail.com>
2024-06-02 13:31:41 +0530

    Please

--- calculator.py
+++ calculator.py
@@ -1,5 +1,5 @@
-a = int(input("Enter the first number: "))
-b = int(input("Enter the second number: "))
+a = int(input("Please Enter the first number: "))
+b = int(input("Please Enter the second number: "))
 print("Sum:", a + b)
 print("Difference:", a - b)
 print("Product:", a * b)
